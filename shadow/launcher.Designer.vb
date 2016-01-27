97a94e7e602d48cd30e775f773789756fa73df24
Pedro Albarracin <p.albarracin@outlook.es>
2016-01-27 17:45:42 +0100

    Revert "Revert "Shadow 270116_1135""
    This reverts commit 77ee5f16b49b18a39b62e195e760eaba33a24aca.

--- shadow/launcher.Designer.vb
+++ shadow/launcher.Designer.vb
@@ -32,6 +32,7 @@ Partial Class launcher
         Me.Button8 = New System.Windows.Forms.Button()
         Me.Button9 = New System.Windows.Forms.Button()
         Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
@@ -124,11 +125,21 @@ Partial Class launcher
         Me.Button10.Text = "Facturar Albaranes"
         Me.Button10.UseVisualStyleBackColor = True
         '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(365, 183)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(139, 43)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "Gestión Gastos"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
         'launcher
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(686, 386)
+        Me.Controls.Add(Me.Button11)
         Me.Controls.Add(Me.Button10)
         Me.Controls.Add(Me.Button9)
         Me.Controls.Add(Me.Button8)
@@ -156,4 +167,5 @@ Partial Class launcher
     Friend WithEvents Button8 As Button
     Friend WithEvents Button9 As Button
     Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
 End Class
